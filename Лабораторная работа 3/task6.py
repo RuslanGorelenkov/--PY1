09968256512c3af9acf60a9ed01759282dcec426
RuslanGorelenkov <115419110+RuslanGorelenkov@users.noreply.github.com>
2022-10-29 21:24:06 +0300

    Update task6.py
    Я поменял count на max_index и сделал для нумерации с конца списка.

--- Лабораторная работа 3/task6.py
+++ Лабораторная работа 3/task6.py
@@ -1,10 +1,11 @@
 list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 max_number = list_numbers[0]
-count = 0
+max_index = 0
 for index, number in enumerate(list_numbers):
     if number > max_number:
         max_number = number
-        count = index
-list_numbers[index], list_numbers[9] = list_numbers[9], list_numbers[index]
+        max_index = index
+list_numbers[max_index], list_numbers[-1] = list_numbers[-1], list_numbers[max_index]
+# -1 это индекс последнего элемента, ели начинать отсчет с конца последовательности
 
 print(list_numbers)
